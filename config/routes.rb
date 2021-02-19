026175cbefa3c4239fae810153937c57815b61de
Caroline Tan <caroline.kh.tan@gmail.com>
2021-02-19 14:08:46 +0800

    index, show, new, create for my Cocktail

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'cocktails/index'
+  get 'cocktails/show'
+  get 'cocktails/new'
+  get 'cocktails/create'
+# User Stories
+# 1. A user can see the list of cocktails
+# 2. A user can see the details of a given cocktail, with the dose needed for each ingredient
+# 3. A user can create a new cocktail
+  root to: 'cocktails#index'
+  resources :cocktails, only: [ :index, :show, :new, :create ]
 end
