e14e9038dfb6caa3a82c6c7c1f3dca71d1a6aa37
Caroline Tan <caroline.kh.tan@gmail.com>
2021-02-22 21:40:18 +0800

    refactored calling cocktail api

--- app/helpers/cocktails_helper.rb
+++ app/helpers/cocktails_helper.rb
@@ -2,35 +2,28 @@ require 'uri'
 require 'json'
 
 module CocktailsHelper
-  def retrieve_instructions(cocktail)
+  def call_cocktail_api(cocktail, requirement)
     base_url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s='
     url = base_url + cocktail.name
     read_url = open(url).read
     parsed = JSON.parse(read_url)
-    @instructions = parsed['drinks'][0]['strInstructions']
+    @results = parsed['drinks'][0]["#{requirement}"]
   end
 
-#   refactor the below to have one render_photo_url vs render_photo_html
-  def render_photo(cocktail, render_html=true)
+  def render_photo_url(cocktail)
     if cocktail.photo.attached?
-      # gives me a url
-      if render_html
-        cl_image_tag cocktail.photo.key, height: 300, width: 400, crop: :fill
-      else
-        cl_image_path cocktail.photo.key, height: 300, width: 400, crop: :fill
-      end
+      cl_image_path cocktail.photo.key, height: 300, width: 400, crop: :fill
     else
-      base_url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s='
-      url = base_url + cocktail.name
-      read_url = open(url).read
-      parsed = JSON.parse(read_url)
-      @photo_url = parsed['drinks'][0]['strDrinkThumb']
-      #gives me raw html
-      if render_html
-        raw("<img src=\"#{@photo_url}\" alt=\"#{@cocktail.name}\" width=\"400\" height=\"400\">")
-      else
-        @photo_url
-      end
+      call_cocktail_api(cocktail, 'strDrinkThumb')
+      @results
     end
   end
-end
+
+  def render_photo_html(cocktail)
+    if cocktail.photo.attached?
+      cl_image_tag cocktail.photo.key, height: 300, width: 400, crop: :fill
+    else
+      call_cocktail_api(cocktail, 'strDrinkThumb')
+      raw("<img src=\"#{@results}\" alt=\"#{@cocktail.name}\" width=\"400\" height=\"400\">")
+    end
+  end
